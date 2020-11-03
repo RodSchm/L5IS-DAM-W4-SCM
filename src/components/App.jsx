41e34ec52c4980c5b226bfd3cf5b0ed24d800a82
RodSchm <rodschmid@outlook.co.nz>
2020-11-03 15:19:49 +1300

    Version 0.4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import Header from "./Header/Header"
 import Footer from "./Footer/Footer"
+import Navbar from "./NavBar/Navbar"
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Navbar />
       <Footer />
     </div>
   );
---
+++ src/components/NavBar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+function Navbar() {
+  return (
+      <div>
+        <ul>
+            <li><a href="shop.html">Shop</a></li>
+            <li><a href="about.html">About Us</a></li>
+            <li><a href="contact.html">Contact</a></li>
+            <li><a href="index.html">Home</a></li>
+        </ul>
+        </div>
+  );
+}
+
+export default Navbar;
